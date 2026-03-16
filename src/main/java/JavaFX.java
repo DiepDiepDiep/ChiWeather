3b41b00e7565ef21f1c1d97b50b21c6dd020e0da
Jonas <jdiep4@uic.edu>
2026-03-16 13:53:10 -0500

    Added attribute names for scene1

--- src/main/java/JavaFX.java
+++ src/main/java/JavaFX.java
@@ -2,8 +2,14 @@ import javafx.application.Application;
 
 import javafx.scene.Scene;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import weather.Period;
 import weather.WeatherAPI;
@@ -16,6 +22,10 @@ public class JavaFX extends Application {
 
 	//Scene 1 attributes
 	Scene s1;
+	VBox s1vbMain, s1vbHour1, s1vbHour2, s1vbHour3, s1vbHour4, s1vbHour5, s1vbHour6, s1vbHour7;
+	HBox s1hbTemp, s1hbWeather, s1hbHourlyTemp, s1hbButton;
+	Button s1bThreeDayForecast;
+
 
 	//Scene 2 attributes
 	Scene s2;
@@ -38,11 +48,16 @@ public class JavaFX extends Application {
 		temperature.setText("Today's weather is: "+String.valueOf(forecast.get(0).temperature));
 		weather.setText(forecast.get(0).shortForecast);
 		
-		
-		
-				
-		s1 = new Scene(new VBox(temperature,weather), 700,700);
+		s1hbTemp = new HBox(20, temperature);
+		s1hbWeather = new HBox(20, weather);
+
+		//Will add more components to main's vbox. Just used to display for now
+		s1vbMain = new VBox(20,s1hbTemp, s1hbWeather);
+		s1 = new Scene(s1vbMain, 700, 700);
 		//End of Scene1
+
+
+
 		//Everything below is start of scene2
 
 
